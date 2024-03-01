20578f5c0beaf1ec8261141556f70638bda1df12
van-bdb <gorohov.ivan.2006@gmail.com>
2024-03-01 23:31:12 +0300

    DZ1

---
+++ 1.bat
@@ -0,0 +1,3 @@
+echo usage example
+1.py -h -i set1 1 2 3 -o set1 -d set1 3 -a set1 stul -o set1
+pause
---
+++ 1.py
@@ -0,0 +1,81 @@
+import sys, json
+
+try:
+    with open("sets.json", "rt") as f:
+        sets = json.load(f)
+    sets = {k:set(v) for k,v in sets.items()}
+except:
+    sets = dict()
+
+
+def on_error(description, task=None):
+    print(f"Error: {description}")
+    if task: print(f"while executing \"{ ' '.join(task) }\"")
+    print("Changes will not be applied. Use -h for help")
+    exit()
+
+def help_(*args):
+    print("""usage: 1.py [{command} [argument]...]...
+commands:
+    -i name [element]...        input set of elements and assign to name "name" 
+    -o name                     output set named "name"
+    -a name element             insert "element" into set named "name"
+    -d name element             delete "element" from set named "name"
+    -h                          help
+    """)
+
+def input_set(name, *elms):
+    sets[name] = set(elms)
+    print(f"\"{name}\" created")
+
+def output_set(name):
+    if sets[name]:
+        set_str = "{"+(', '.join(sets[name]))+"}"
+    else:
+        set_str = "\u2205"
+    print(f"{name}={set_str}")
+    
+def add_to_set(name, elm):
+    sets[name].add(elm)
+
+def del_from_set(name, elm):
+    try:
+        sets[name].remove(elm)
+    except KeyError:
+        print(f"NIL: {elm} not in set {name}")
+ 
+actions = {
+    "-i": input_set,
+    "-o": output_set,
+    "-a": add_to_set,
+    "-d": del_from_set,
+    "-h": help_,
+    None: lambda x:None, # for triggering task execution before exit
+    }
+
+def parse(args):
+    task = [args[0]]
+    for arg in args[1:]:
+        if arg in actions:
+            try:
+                actions[task[0]](*task[1:])
+            except TypeError:
+                on_error("wrong amount of arguments", task)
+            except KeyError:
+                on_error("no set with such name", task)
+            except ValueError:
+                on_error("no set with such name", task)
+            task.clear()
+        task.append(arg)
+    return
+
+if __name__ == "__main__":
+    args = list(sys.argv[1:])+[None]
+    if len(args)<=1:
+        print("No arguments provided. Use -h for help")
+    elif args[0] not in actions:
+        on_error(f"first argument \"{args[0]}\" is not valid command")
+    parse(args)
+    if sets:
+        with open("sets.json", "wt") as f:
+            json.dump({k:list(v) for k,v in sets.items()}, f)
